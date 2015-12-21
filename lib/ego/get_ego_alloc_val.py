0261a109501a5a0b21d0de74126c2132208d3800
pxy0592 <pxy0592@gmail.com>
2015-12-21 15:46:15 +0800

    issue#11: add get EGO allocation function

--- lib/ego/get_ego_alloc_val.py
+++ lib/ego/get_ego_alloc_val.py
@@ -1,34 +1,31 @@
 #!/usr/bin/env python
 
 import sys
+import json
 
 def csv2json(_fp, _key):
   ''' TODO: speicify user-defined name as primary key'''
   #the columns are on the first line separated by ;
-  columns = _fp.readline().replace('"', '').split(",");
+  columns = _fp.readline().strip('\n').replace('"', '').split(",")
   objs = {}
-  rows = _fp.readlines();
+  rows = _fp.readlines()
   for row in rows:
-  	fields = row.replace('"', '').split(",")
-  	obj = {}
-  	for column in columns:
-  		obj[column.replace("\n","")] = fields[columns.index(column)].replace("\n","")	 
-  	objs[fields[columns.index(_key)]] = obj
+    fields = row.strip('\n').replace('"', '').split(",")
+    obj = {}
+    for column in columns:
+      obj[column] = fields[columns.index(column)]
+      objs[fields[columns.index(_key)]] = obj
   
-  return objs, fields[columns.index(_key)]
-
-def get_value(_cnt, _key_val, _name):
-  ''' TODO: implement this function to get value of specified name in specified key'''
-  return _cnt[_key_val][_name]
+  return json.dumps(objs)
 
 if __name__ == "__main__":
   ''' TODO: handle multiple lines of same alloc id or same client name '''
   csv_file = sys.argv[1]
-  primary_key = sys.argv[2]
+  primary_key = sys.argv[2] # content format is key,val
   name = sys.argv[3]
 
   fp = open(csv_file, 'r')
-  dobj,dval =  csv2json(fp, primary_key)
-  val = get_value(dobj, dval, name)
-  print "{0}:{1}".format(name, val)
+  dkey = primary_key.replace('"', '').split(',')
+  dobj =  csv2json(fp, dkey[0])
+  print "{0}:{1}".format(name, json.loads(dobj)[dkey[1]][name])
 
