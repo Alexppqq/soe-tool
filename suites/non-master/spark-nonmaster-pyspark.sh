141eba2376529c1aa45cee8b8a197200d9749c41
alexppqq <342667859@qq.com>
2015-12-24 00:55:23 -0500

    issue#26:3 cases for nonmaster

---
+++ suites/non-master/spark-nonmaster-pyspark.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#source env
+source $TEST_TOOL_HOME/conf/environment.conf
+source $TEST_TOOL_HOME/lib/framework.func
+source $TEST_TOOL_HOME/lib/worklaod.func
+#case filter
+ca_filter_only_singleHost
+
+#run scenario
+source $TEST_TOOL_HOME/scenario/scenario_fifo_conf
+
+#run case
+echo "$val_case_name - begin" 
+echo "$val_case_name - sbumit job"
+# 3 task each run 10s
+ca_spark_pyspark_run_sleep  3 10 &>> $val_case_log_dir/tmpOut 
+sleep 5
+#tmpOut=`ca_find_by_key_word $val_case_log_dir/tmpOut "onStageCompleted: stageId(0)"`
+#lineOutput=`ca_find_by_key_word $val_case_log_dir/tmpOut "onStageCompleted: stageId(0)"|wc -l`
+ca_assert_file_contain_key_word $val_case_log_dir/tmpOut "onStageCompleted: stageId(0)" "nonmaster pyspark sleep job failed"
+echo "$val_case_name - write report"
+#ca_assert_num_ge $lineOutput 1 "job not done."
+echo "$val_case_name - end" 
+ca_recover_and_exit 0;
+
--- suites/shuffle-service/word2vec.sh
+++ suites/shuffle-service/word2vec.sh
@@ -17,14 +17,21 @@ source $TEST_TOOL_HOME/scenario/scenario_fifo_conf
 #run case
 echo "$val_case_name - begin" 
 echo "$val_case_name - sbumit job"
-`hdfs dfs -mkdir -p /user/root `
-`hdfs dfs -put $TEST_TOOL_HOME/data/text8_lines /user/root/`
+randomKey=`date +%s`
+inputFile="/input-${randomKey}.txt"
+$HADOOP_HOME/bin/hadoop fs -copyFromLocal $TEST_TOOL_HOME/data/text8_lines $inputFile
+#verify input
+$HADOOP_HOME/bin/hadoop fs -ls $inputFile
+
+#`hdfs dfs -mkdir -p /user/root `
+#`hdfs dfs -put $TEST_TOOL_HOME/data/text8_lines /user/root/`
 ca_kill_shuffle_service_process
 ca_stop_shuffle_service_by_ego_service
 sleep 25
 ca_start_shuffle_service_by_ego_service
-$SPARK_HOME/bin/spark-submit --driver-memory 4g $SPARK_HOME/examples/src/main/python/mllib/word2vec.py text8_lines &>>$val_case_log_dir/tmpOut
-`hdfs dfs -rm -f -r /user/root/text8_lines &>> /dev/null`
+$SPARK_HOME/bin/spark-submit --driver-memory 4g $SPARK_HOME/examples/src/main/python/mllib/word2vec.py $inputFile &>>$val_case_log_dir/tmpOut
+#`hdfs dfs -rm -f -r /user/root/text8_lines &>> /dev/null`
+$HADOOP_HOME/bin/hadoop fs -rm -r $inputFile &>> /dev/null
 sleep 25
 ca_assert_file_contain_key_word $val_case_log_dir/tmpOut "taiwan" "word2vec failed"
 echo "$val_case_name - write report"
