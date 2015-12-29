80661970585acff2248d35ce6a8521f461d075e9
alexppqq <342667859@qq.com>
2015-12-29 03:22:11 -0500

    issue#31:add function ca_keep_check_in_file

--- suites/shuffle-service/sparkTC.sh
+++ suites/shuffle-service/sparkTC.sh
@@ -15,18 +15,31 @@ source $TEST_TOOL_HOME/scenario/scenario_fifo_conf
 echo "$val_case_name - begin" 
 echo "$val_case_name - sbumit job"
 ca_kill_shuffle_service_process
+sleep 3
 ca_stop_shuffle_service_by_ego_service
-sleep 5
+sleep 7
 ca_start_shuffle_service_by_ego_service
+sleep 10
+
+egosh service list -ll > $TEST_TOOL_HOME/data/service.csv
+shuffle=$( python $TEST_TOOL_HOME/lib/ego/get_ego_alloc_val.py $TEST_TOOL_HOME/data/service.csv "SERVICE,SPARKSS" 'INST_STATE' )
+echo $shuffle
+[[ $shuffle != "INST_STATE:RUN"  ]] && echo "start shuffle service failed" && ca_recover_and_exit 1
+
+
 $SPARK_HOME/bin/spark-submit --conf spark.master=spark://$SYM_MASTER_HOST:7077  --deploy-mode cluster  --class org.apache.spark.examples.SparkTC $SPARK_HOME/lib/spark-examples*  &>> $val_case_log_dir/tmpOut
-sleep 50
+
+sleep 5
 driverStatus=`ca_get_akka_driver_status $val_case_log_dir/tmpOut`
 echo "$val_case_name - driver status: $driverStatus"
 drivername=`ca_get_akka_driver_name $val_case_log_dir/tmpOut`
 echo "$val_case_name - driver name: $drivername" 
-sleep 20
+
+ca_keep_check_in_file "edges" "$SPARK_HOME/work/$drivername/stdout"  "1" "100"
 ca_assert_file_contain_key_word $SPARK_HOME/work/$drivername/stdout  "edges" "sparkTC failed"
 echo "$val_case_name - write report"
 echo "$val_case_name - end" 
+
+rm -rf $TEST_TOOL_HOME/data/service.csv
 ca_stop_shuffle_service_by_ego_service
 ca_recover_and_exit 0;
--- suites/shuffle-service/word2vec.sh
+++ suites/shuffle-service/word2vec.sh
@@ -23,18 +23,27 @@ $HADOOP_HOME/bin/hadoop fs -copyFromLocal $TEST_TOOL_HOME/data/text8_lines $inpu
 #verify input
 $HADOOP_HOME/bin/hadoop fs -ls $inputFile
 
-#`hdfs dfs -mkdir -p /user/root `
-#`hdfs dfs -put $TEST_TOOL_HOME/data/text8_lines /user/root/`
 ca_kill_shuffle_service_process
+sleep 3
 ca_stop_shuffle_service_by_ego_service
-sleep 25
+sleep 7
 ca_start_shuffle_service_by_ego_service
+sleep 10
+
+egosh service list -ll > $TEST_TOOL_HOME/data/service.csv
+shuffle=$( python $TEST_TOOL_HOME/lib/ego/get_ego_alloc_val.py $TEST_TOOL_HOME/data/service.csv "SERVICE,SPARKSS" 'INST_STATE' )
+echo $shuffle
+[[ $shuffle != "INST_STATE:RUN"  ]] && echo "start shuffle service failed" && ca_recover_and_exit 1
+
+
 $SPARK_HOME/bin/spark-submit --driver-memory 4g $SPARK_HOME/examples/src/main/python/mllib/word2vec.py $inputFile &>>$val_case_log_dir/tmpOut
-#`hdfs dfs -rm -f -r /user/root/text8_lines &>> /dev/null`
-$HADOOP_HOME/bin/hadoop fs -rm -r $inputFile &>> /dev/null
-sleep 25
+ca_keep_check_in_file "taiwan" "$val_case_log_dir/tmpOut"  "1" "100"
 ca_assert_file_contain_key_word $val_case_log_dir/tmpOut "taiwan" "word2vec failed"
 echo "$val_case_name - write report"
 echo "$val_case_name - end" 
+
+
 ca_stop_shuffle_service_by_ego_service
+$HADOOP_HOME/bin/hadoop fs -rm -r $inputFile &>> /dev/null
+rm -rf $TEST_TOOL_HOME/data/service.csv
 ca_recover_and_exit 0;
